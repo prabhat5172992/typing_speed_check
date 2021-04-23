52d4b0f59ad597ae07b3fec4bcd3a1d2d1425cb6
Prabhat Ranjan <prabhat5172992@gmail.com>
2021-04-24 00:31:36 +0530

    First round QA bugs fixed

--- src/typingSpeedCheck.jsx
+++ src/typingSpeedCheck.jsx
@@ -5,31 +5,31 @@ class TypingSpeedCheck extends Component {
   constructor() {
     super();
     this.state = {
-      countW: 0,
-      countC: 0,
-      countWS: 0,
-      t1: 0,
+      countW: 0, // count word
+      countC: 0, // count characters
+      countWS: 0, // char count without spaces
+      t1: 0, // timer
       textContent: "",
-      typingOnOFF: false,
-      rp: Content["data"]["text1"],
-      leftRP: Content["data"]["text1"],
-      tStSP: false,
+      typingOnOFF: false, // To disable textarea when timer shows 60 s
+      rp: Content["data"]["text1"], // random paragraph
+      leftRP: Content["data"]["text1"], // random paragraph left to type
+      tStSP: false, // typing status to start timer once started it will be true
       typingSpeed: 0,
-      typed: "",
-      errors: "",
+      typed: "", // The paragraph which is already typed correctly
+      errors: "", // Paragraph typed with error
     };
   }
   // timer
   timeCounter = () => {
     let t = 0;
     this.counter = setInterval(() => {
-      if (t < 60) {
+      if (t < 120) {
         t += 1;
         this.setState({
           t1: t,
         });
       }
-      if (t === 60) {
+      if (t === 120) {
         this.setState({ typingOnOFF: true });
       }
     }, 1000);
@@ -69,6 +69,7 @@ class TypingSpeedCheck extends Component {
       typingSpeed: 0,
     });
   };
+
   checkForCorrection = (para) => {
     const { rp, typed, errors, leftRP } = this.state;
     const gp = rp.split("");
@@ -104,51 +105,66 @@ class TypingSpeedCheck extends Component {
       }
     }
   };
+
   getCharCount = (e) => {
-    const { textContent, tStSP, countW, t1 } = this.state;
+    const { textContent, tStSP, typed, t1, rp } = this.state;
     let { countC } = this.state;
     if (!textContent && !tStSP) {
       this.setState({ tStSP: true });
       this.timeCounter();
     }
     const val = e.target.value;
-    this.checkForCorrection(val);
-    this.setState({
-      textContent: val,
-      typingSpeed: t1 ? Math.floor((countW / t1) * 60) : 1,
-    });
-    let ws = 0;
-    if (val) {
-      // Char count with space
-      if (val.toString().length < countC) {
-        countC -= 1;
-      } else {
-        countC += 1;
-      }
-      // Char count without space
-      for (let s of val) {
-        ws += s.trim().length;
-      }
-      this.setState({ countC, countWS: ws });
-      // Word count
-      const wordArr = val.toString().split(" ");
-      if (wordArr.length) {
-        let wc = 0;
-        for (let w of wordArr) {
-          if (w && w.includes("\n")) {
-            wc += this.checkNewLine(w);
-          } else if (w) {
-            wc += 1;
-          } else {
-            wc += 0;
-          }
+    const typedCount = typed ? typed.split(" ").length : 0;
+
+    if (val.length <= rp.length) {
+      this.setState(
+        {
+          textContent: val,
+          typingSpeed: t1 ? Math.floor((typedCount / t1) * 60) : 1,
+        },
+        () => {
+          this.checkForCorrection(this.state.textContent);
         }
-        this.setState({ countW: wc });
+      );
+
+      let ws = 0;
+      if (val) {
+        // Char count with space
+        if (val.toString().length < countC) {
+          countC -= 1;
+        } else {
+          countC += 1;
+        }
+        // Char count without space
+        for (let s of val) {
+          ws += s.trim().length;
+        }
+        this.setState({ countC, countWS: ws });
+        // Word count
+        const wordArr = val.toString().split(" ");
+        if (wordArr.length) {
+          let wc = 0;
+          for (let w of wordArr) {
+            if (w && w.includes("\n")) {
+              wc += this.checkNewLine(w);
+            } else if (w) {
+              wc += 1;
+            } else {
+              wc += 0;
+            }
+          }
+          this.setState({ countW: wc });
+        }
+      } else {
+        this.clearCount();
       }
-    } else {
-      this.clearCount();
     }
   };
+
+  disablePaste(e) {
+    e.preventDefault();
+  }
+
   render() {
     const {
       countW,
@@ -163,7 +179,7 @@ class TypingSpeedCheck extends Component {
       typingSpeed,
     } = this.state;
     return (
-      <div>
+      <div className="typs-wrapper">
         <h1 className="typeStH1">{`Your Typing Speed: ${typingSpeed} wpm`}</h1>
         <div className="typingInfoWrapper">
           <div className="infoWrapper">
@@ -178,10 +194,15 @@ class TypingSpeedCheck extends Component {
             {leftRP}
           </div>
         </div>
+        <h3>
+          !! Please type each characters correctly, also correct the red one and
+          make it green. Typing speed includes only green ones. !!
+        </h3>
         <textarea
           className="typingSPCH blinking-cursor"
           value={textContent}
           onChange={(e) => this.getCharCount(e)}
+          onPaste={(e) => this.disablePaste(e)}
           disabled={typingOnOFF}
         />
         <button
